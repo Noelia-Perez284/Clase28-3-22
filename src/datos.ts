5f860e45f79dd634534b7605de2b398e5fa04242
Noelia-Perez284 <102192324+Noelia-Perez284@users.noreply.github.com>
2022-03-31 20:46:08 -0300

    ejercicio de clase 31-03 en clase

--- src/datos.ts
+++ src/datos.ts
@@ -1,11 +1,34 @@
 let btnEnviar = document.getElementById("btnEnviar");
-let rotulo = document.getElementById("rotulo");
-let dato = <HTMLInputElement>document.getElementById("dato");
+let rotulo1 = document.getElementById("rotulo1");
+let dato1 = <HTMLInputElement>document.getElementById("dato1");
+let rotulo2 = document.getElementById("rotulo2");
+let dato2 = <HTMLInputElement>document.getElementById("dato2");
+
+
+
 btnEnviar.addEventListener("click", () => {
-  console.log("ingreso " + dato.value);
-  let nombre = dato.value;
+  let base = Number(dato1.value);
+  let altura = Number(dato2.value);
+  let area = base * altura
+  console.log("el area es" base * altura);
+  
+  
   // Acá va nuestro código
-  console.log(nombre);
+  
   //dato.value = "hola" ;
   // console.log("rotulo");
 }); //
+
+
+
+//let btnEnviar = <HTMLButtonElement>document.getElementById("btnEnviar");
+//let dato = <HTMLInputElement>document.getElementById("dato");
+
+//let rotulo = <HTMLParagraphElement>document.getElementById("rotulo");
+//let nombre: string;
+
+//btnEnviar.addEventListener("click", () => {
+  // Acá va nuestro código
+  //rotulo.innerHTML = "Dato"
+  //nombre = dato.value;
+//});
